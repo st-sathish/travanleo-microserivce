3106b349c4307e5b79a187078ed7c92c5b835638
Sathish <sathish.2009btech@gmail.com>
2020-06-26 10:55:40 +0530

    rename JpaCommandRepository to JpaCommandSourceRepository

--- user-service/src/main/java/com/travanleo/user/commands/domain/JpaCommandSourceRepository.java
+++ user-service/src/main/java/com/travanleo/user/commands/domain/JpaCommandSourceRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaCommandRepository extends JpaRepository<CommandSource, Long> {
+public interface JpaCommandSourceRepository extends JpaRepository<CommandSource, Long> {
 
 }
