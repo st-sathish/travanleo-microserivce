3a446700ba533929a1b271bbd936335d025356f3
Sathish <sathish.2009btech@gmail.com>
2020-07-01 08:42:31 +0530

    added spring cloud security in comment service

---
+++ auth-service/src/main/resources/db/migration/V4__OAuth_Comment_Service_Client_Credentials.sql
@@ -0,0 +1,21 @@
+-- encoded password is 'secret'
+INSERT INTO
+  oauth_client_details (
+    client_id,
+    client_secret,
+    resource_ids,
+    scope,
+    authorized_grant_types,
+    access_token_validity,
+    refresh_token_validity
+  )
+VALUES
+  (
+    'appcommentclient',
+    '$2a$10$F2dXfNuFjqezxIZp0ad5OeegW43cRdSiPgEtcetHspiNrUCi3iI6O',
+    'commentService',
+    'read,write',
+    'authorization_code,check_token,refresh_token,password',
+    1000000,
+    1000000
+  );
