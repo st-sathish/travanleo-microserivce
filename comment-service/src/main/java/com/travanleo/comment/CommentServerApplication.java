10ea9706c9cdfe31257e721a49f998d2bf7ecb51
Sathish <sathish.2009btech@gmail.com>
2020-07-01 10:09:03 +0530

    make comment service as resource server

--- comment-service/src/main/java/com/travanleo/comment/CommentServerApplication.java
+++ comment-service/src/main/java/com/travanleo/comment/CommentServerApplication.java
@@ -4,10 +4,12 @@ import com.travanleo.comment.boot.AbstractApplicationConfiguration;
 import com.travanleo.comment.boot.db.DataSourceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 public class CommentServerApplication {
 
     @Import(DataSourceConfiguration.class)
+    @EnableResourceServer
     private static class Configuration extends AbstractApplicationConfiguration{ }
 
     public static void main(String[] args) {
